84111602a50fa4b87a4c6753be0de61605171266
JXieDS <jxie_98@yahoo.com>
2014-10-28 07:36:09 -0700

    Include 3 .R files with assignment templates

---
+++ rankall.R
@@ -0,0 +1,24 @@
+## rankall function takes two arguments: an outcome name (outcome) and a hospital ranking (num).
+## The function reads the outcome-of-care-measures.csv file and returns a 2-column data frame 
+## containing the hospital in each state that has the ranking specified in num.
+## The function should return a value for every state (some may be NA). The first column in data
+## frame is named hospital, the second column is named state, containing the 2-char abbrev. for the 
+## state name. Hospital that don't have data on a particular outcome should be excluded from the set
+## of hospitals when deciding the rankings.
+
+rankall <- function(outcome, num = "best") {
+  
+        ## Read outcome data
+  
+  
+        ## Check that state adn outcome are valid
+  
+  
+        ## For each state, find the hospital of the given rank
+  
+  
+        ## Return a  data frame with teh hospital names adn the abbrev. state name
+  
+  
+  
+}
---
+++ rankhospital.R
@@ -0,0 +1,17 @@
+## rankhospital function takes three arguments: the 2-character abbreviated name of a
+## state (state), an outcome (outcome), and the ranking of a hospital in that state for that outcome
+## (num).
+## The function reads the outcome-of-care-measures.csv file and returns a character vecter with the
+## name of the hospital that has the ranking specified by the num argument
+
+rankhospital <- function(state, outcome, num = "best"){
+        ## Read outcome data
+  
+  
+        ## Check that state adn outcome are valid
+  
+  
+        ## Return hospital name in that state with the given rank
+        ## 30-day death rate
+  
+}
